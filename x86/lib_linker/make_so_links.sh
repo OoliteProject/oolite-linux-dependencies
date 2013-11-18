fe02d45dc84c1b194e2929b2db83c3d2c8e45ae8
KonstantinosSykas <getafix@oolite.org>
2013-11-19 00:36:05 +0200

    Linux dependencies update for transition to OpenAL

--- x86/lib_linker/make_so_links.sh
+++ x86/lib_linker/make_so_links.sh
@@ -3,7 +3,7 @@
 ln -s ../lib/libespeak.so.1 libespeak.so
 ln -s ../lib/libpng14.so.14 libpng14.so
 ln -s ../lib/libSDL-1.2.so.0 libSDL.so
-ln -s ../lib/libSDL_mixer-1.2.so.0 libSDL_mixer.so
+ln -s ../lib/libopenal.so.1.15.1 libopenal.so
 
 exit
 
