27add875b05846f12f8c12ab0f376a464d43927b
KonstantinosSykas <getafix@oolite.org>
2013-11-20 22:30:28 +0200

    Fixed names of external library dependencies needed by OpenAL

--- x86_64/lib_linker/make_so_links.sh
+++ x86_64/lib_linker/make_so_links.sh
@@ -4,9 +4,9 @@ ln -s ../lib/libespeak.so.1 libespeak.so
 ln -s ../lib/libpng14.so.14 libpng14.so
 ln -s ../lib/libSDL-1.2.so.0 libSDL.so
 ln -s ../lib/libopenal.so.1.15.1 libopenal.so
-ln -s ../lib/libogg.so.0.8.0 libogg.so
-ln -s ../lib/libvorbis.so.0.4.6 libvorbis.so
-ln -s ../lib/libvorbisfile.so.3.3.5 libvorbisfile.so
+ln -s ../lib/libogg.so.0 libogg.so
+ln -s ../lib/libvorbis.so.0 libvorbis.so
+ln -s ../lib/libvorbisfile.so.3 libvorbisfile.so
 
 exit
 
