be45fde17fc3cb81ab4137d5c20f322f3a2b9a23
KonstantinosSykas <getafix@oolite.org>
2013-11-21 00:55:41 +0200

    Added zlib library for zipped-oxp support

--- x86_64/lib_linker/make_so_links.sh
+++ x86_64/lib_linker/make_so_links.sh
@@ -7,6 +7,7 @@ ln -s ../lib/libopenal.so.1 libopenal.so
 ln -s ../lib/libogg.so.0 libogg.so
 ln -s ../lib/libvorbis.so.0 libvorbis.so
 ln -s ../lib/libvorbisfile.so.3 libvorbisfile.so
+ln -s ../lib/libz.so.1 libz.so
 
 exit
 
